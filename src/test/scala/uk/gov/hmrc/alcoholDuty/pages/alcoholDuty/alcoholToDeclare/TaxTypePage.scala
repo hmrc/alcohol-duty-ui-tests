70dde3041fe302ff1b489ce11f29fe41f11d40d3
kavita.d.kumari <kavita.d.kumari@accenture.com>
2024-01-12 03:08:16 +0000

    ADR-748 WIP

---
+++ src/test/scala/uk/gov/hmrc/alcoholDuty/pages/alcoholDuty/alcoholToDeclare/TaxTypePage.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.alcoholDuty.pages.alcoholDuty.alcoholToDeclare
+
+import uk.gov.hmrc.alcoholDuty.conf.TestConfiguration
+import uk.gov.hmrc.alcoholDuty.pages.BasePage
+import uk.gov.hmrc.alcoholDuty.pages.alcoholDuty.alcoholToDeclare.DraughtReliefQuestionPage.cssSelector
+import uk.gov.hmrc.alcoholDuty.pages.alcoholDuty.dutySuspendedDeliveries.DutySuspendedDeliveriesCheckYourAnswersPage.cssSelector
+
+object TaxTypePage extends BasePage {
+
+  override val url: String = TestConfiguration.url("alcohol-duty-returns-frontend") + "/taxType"
+  override val title       = "Your product’s tax type code"
+
+  override def expectedPageErrorTitle: Option[String] = Some(
+    "Error: Your product’s tax type code - Manage your Alcohol Duty - GOV.UK"
+  )
+
+  override def expectedPageTitle: Option[String] = Some(
+    "Your product’s tax type code - Manage your Alcohol Duty - GOV.UK"
+  )
+
+  override def expectedPageHeader: Option[String] = Some("Your product’s tax type code")
+
+  override def clickRadioButton(text: String): Unit = {
+    text match {
+      case "Beer" => click on cssSelector("input[value='301_Beer']")
+    }
+  }
+}
