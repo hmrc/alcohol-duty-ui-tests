3b399e8b22fa9857f8355ee16c4b4f9d5345a060
kavita.d.kumari <kavita.d.kumari@accenture.com>
2024-10-15 12:04:21 +0100

    ADR-1394: WIP

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,12 +4,12 @@ object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
     "org.scalatest"       %% "scalatest"              % "3.2.17"   % Test,
-    "io.cucumber"         %% "cucumber-scala"         % "8.20.0"   % Test,
+    "io.cucumber"         %% "cucumber-scala"         % "8.25.0"   % Test,
     "io.cucumber"          % "cucumber-junit"         % "7.15.0"   % Test,
     "io.cucumber"          % "cucumber-picocontainer" % "7.15.0"   % Test,
     "junit"                % "junit"                  % "4.13.2"   % Test,
     "com.novocode"         % "junit-interface"        % "0.11"     % Test,
-    "uk.gov.hmrc"         %% "ui-test-runner"         % "0.30.0"   % Test,
+    "uk.gov.hmrc"         %% "ui-test-runner"         % "0.38.0"   % Test,
     "org.scalatestplus"   %% "selenium-4-12"          % "3.2.17.0" % Test,
     "com.typesafe.play"   %% "play-json-joda"         % "2.10.4"   % Test,
     "com.typesafe"         % "config"                 % "1.4.2"    % Test,
