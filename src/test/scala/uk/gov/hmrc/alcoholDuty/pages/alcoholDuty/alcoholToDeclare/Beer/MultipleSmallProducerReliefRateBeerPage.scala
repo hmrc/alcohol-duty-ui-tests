7b89e6282be5f256642968288b4fe042254d44f0
chiransandi <chiran.san01@gmail.com>
2024-12-13 17:44:28 +0000

    ADR-1579 - Content updated

--- src/test/scala/uk/gov/hmrc/alcoholDuty/pages/alcoholDuty/alcoholToDeclare/Beer/MultipleSmallProducerReliefRateBeerPage.scala
+++ src/test/scala/uk/gov/hmrc/alcoholDuty/pages/alcoholDuty/alcoholToDeclare/Beer/MultipleSmallProducerReliefRateBeerPage.scala
@@ -25,18 +25,18 @@ object MultipleSmallProducerReliefRateBeerPage extends BasePage {
     TestConfiguration.url("alcohol-duty-returns-frontend") + "/complete-return/alcoholic-products/Beer/declare/spr/multiple-duty-rates/eligible-volume"
   override val newUrl: String =
     TestConfiguration.url("alcohol-duty-returns-frontend") + "/complete-return/alcoholic-products/Beer/change/spr/multiple-duty-rates/eligible-volume"
-  override val title          = "Tell us about the beer you need to declare that are eligible for Small Producer Relief"
+  override val title          = "Tell us about the beer that is eligible for Small Producer Relief"
 
   override def expectedPageErrorTitle: Option[String] = Some(
-    "Error: Tell us about the beer you need to declare that are eligible for Small Producer Relief - Manage your Alcohol Duty - GOV.UK"
+    "Error: Tell us about the beer that is eligible for Small Producer Relief - Manage your Alcohol Duty - GOV.UK"
   )
 
   override def expectedPageTitle: Option[String] = Some(
-    "Tell us about the beer you need to declare that are eligible for Small Producer Relief - Manage your Alcohol Duty - GOV.UK"
+    "Tell us about the beer that is eligible for Small Producer Relief - Manage your Alcohol Duty - GOV.UK"
   )
 
   override def expectedPageHeader: Option[String] = Some(
-    "Tell us about the beer you need to declare that are eligible for Small Producer Relief"
+    "Tell us about the beer that is eligible for Small Producer Relief"
   )
 
   override def clickRadioButton(text: String): Unit =
--- src/test/scala/uk/gov/hmrc/alcoholDuty/pages/alcoholDuty/dutySuspendedDeliveries/DeclareDutySuspendedDeliveriesPage.scala
+++ src/test/scala/uk/gov/hmrc/alcoholDuty/pages/alcoholDuty/dutySuspendedDeliveries/DeclareDutySuspendedDeliveriesPage.scala
@@ -23,17 +23,17 @@ object DeclareDutySuspendedDeliveriesPage extends BasePage {
 
   override val url: String =
     TestConfiguration.url("alcohol-duty-returns-frontend") + "/complete-return/duty-suspended-deliveries/do-you-need-to-report"
-  override val title       = "Do you need to tell us about any duty suspended alcohol?"
+  override val title       = "Have you delivered or received any duty suspended finished alcoholic product?"
 
   override def expectedPageErrorTitle: Option[String] = Some(
-    "Error: Do you need to tell us about any duty suspended alcohol? - Manage your Alcohol Duty - GOV.UK"
+    "Error: Have you delivered or received any duty suspended finished alcoholic product? - Manage your Alcohol Duty - GOV.UK"
   )
 
   override def expectedPageTitle: Option[String] = Some(
-    "Do you need to tell us about any duty suspended alcohol? - Manage your Alcohol Duty - GOV.UK"
+    "Have you delivered or received any duty suspended finished alcoholic product? - Manage your Alcohol Duty - GOV.UK"
   )
 
-  override def expectedPageHeader: Option[String] = Some("Do you need to tell us about any duty suspended alcohol?")
+  override def expectedPageHeader: Option[String] = Some("Have you delivered or received any duty suspended finished alcoholic product?")
 
   override def clickRadioButton(text: String): Unit =
     text match {
