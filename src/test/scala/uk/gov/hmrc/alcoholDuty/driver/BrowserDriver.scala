9cd25d42b8254be4c29ea2ad722d7bad82f92fc7
Psingh226 <189774081+Psingh226@users.noreply.github.com>
2025-11-10 13:55:43 +0000

    Added implicit driver instace

--- src/test/scala/uk/gov/hmrc/alcoholDuty/driver/BrowserDriver.scala
+++ src/test/scala/uk/gov/hmrc/alcoholDuty/driver/BrowserDriver.scala
@@ -20,8 +20,14 @@ import com.typesafe.scalalogging.LazyLogging
 import org.openqa.selenium.WebDriver
 import uk.gov.hmrc.selenium.webdriver.Driver
 
+import com.typesafe.scalalogging.LazyLogging
+import org.openqa.selenium.WebDriver
+import uk.gov.hmrc.selenium.webdriver.Driver
+
 trait BrowserDriver extends LazyLogging {
+  logger.info(
+    s"Instantiating Browser: ${sys.props.getOrElse("browser", "'browser' System property not set. This is required")}"
+  )
 
-  implicit val driver: WebDriver = Driver.instance
-
+  implicit def driver: WebDriver = Driver.instance
 }
